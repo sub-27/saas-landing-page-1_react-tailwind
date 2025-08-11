7cb4a056bb05540eb768a618d3fdd9fb389ac574
sub-27 <sbhawt@gmail.com>
2025-08-11 23:44:38 +0530

    services section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Layout } from "./components/Layout";
+import { Brands } from "./components/sections/Brands";
 import { Hero } from "./components/sections/Hero";
+import { Services } from "./components/sections/Services";
 
 function App() {
   return (
     <Layout title="EdgeAI">
       <Hero />
+      <Brands />
+      <Services />
     </Layout>
   );
 }
