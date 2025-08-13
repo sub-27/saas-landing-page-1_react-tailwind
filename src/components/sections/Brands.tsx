49ef1066eeac24f10f1383cd0dae117be3db9f2a
sub-27 <sbhawt@gmail.com>
2025-08-13 13:32:47 +0530

    changes

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -18,7 +18,7 @@ export const Brands = () => {
               className="p-4 sm:p-5 rounded-xl bg-body border border-box-border group"
             >
               <img
-                src={`/assets/logos/${logo}.png`}
+                src={`src/public/assets/logos/${logo}.png`}
                 alt={logo}
                 width="100"
                 height="60"
